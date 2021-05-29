fe5dcaa673c64d3a150075608c52eed2c216b0f6
Humblelecturer <64842683+Humblelecturer@users.noreply.github.com>
2021-05-29 11:10:15 +0100

    Add files via upload

---
+++ USSD assignment.py
@@ -0,0 +1,140 @@
+#Wema bank USSD
+print ("please enter your bank USSD")
+option= input("")
+if (option=="*945#"):
+    print("1.Open Account\n2.Airtime/Data\n3.Transfer\n4.Cable TV\n5.Internet \n6.Electricity Bill\n7.Quick Loan\n99.Next")
+else: print ("It seems you dialed a Wrong code!\n'*945#' is the WEMA bank USSD code")
+option = input("")
+if (option == "1"):
+	print("Type your BVN")
+	BVN =input("")
+	name=input("Your account name is 'MUHAMMAD SHEHU'\nType '1' to register")
+	option=input ("")
+if (option== "1"):
+    print ("You have successfully opened Account with WEMA Bank, kindly wait for 24hrs to receive your Account number")
+elif (option==["2,3,4,5,6,7,8,9,0"]):
+     print ("wrong input")
+     option = input("")
+if (option=="2"):
+	print ("Buy Airtime and Data\n1.Buy Airtime Self\n2.Buy Airtime Other\n3.Data Self\n4.Data Other\n0.Main Menu")
+	option =input ("")
+if (option=="1"):
+	print ("Input amount")
+	amount=input("")
+	print("input your four digits pin")
+	pin =input ("")
+	print ("You have successfully recharged Airtime from your WEMA bank account")
+	option = input ("")
+if (option =="2"):
+	print ("Enter recipient Number")
+	option = input ("")
+	print ("Enter amount")
+	amount= input ("")
+	print ("Enter your four digits pin")
+	option= input ("")
+if (option== "2244"):   		
+		print ("You are about to recharge Airtime for 0xxxxxxxxx: press '1' to continue")
+		option = input("")
+if (option == "1"):
+	print ("You've successfully recharged an Airtime worth xxxx to 0xxxxxxxx. Thank you!")
+	option =input ("")
+if (option =="3"):
+	print ("MTN Data Plan\n1.Daily Plans\n2.Weekly Plans\n3.Monthly Plans\n4.60 Days Plans\n5.90 Days Plans\n6.Yearly Plan\n0.Back")
+	option= input("")
+if (option=="1"):
+	print ("Enter amount")
+	option=input ("")
+	option=input ("Enter your four digits pin")
+if (option =="2244"):
+	print ("You've successfully purchased a Daily plan. Thank you!")
+	option= input ("")
+if (option == "2"):
+	print ("MTN Weekly Plans\n1.350MB = N300\n2.1GB=N500\n3.6GB=N1500\n0.Back")
+	option = input ("")
+if (option== "1"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option == "2244"):
+	print ("You've successfully purchased a  Weekly Data plan. Thank you!")
+	option=input ("")
+if (option=="2"):
+	print ("Enter your four digits pin")
+	option= input("")
+if (option=="2244"):
+	print("You've successfully purchased a Weekly Data plan. Thank you!")
+	
+	option= input ("")
+if (option == "3"):
+	print ("Monthly Plans\n1.1.5GB=N1000\n2.2GB=N1200\n3.3GB=1500\n4.4.5GB=N2000\n5.6GB=N2500\n6.8GB=3000\n7.10GB=3500\n99.Next")
+	option = input ("")
+if (option=="1"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="2"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="3"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="3"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		option = input ("")
+if (option=="4"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="5"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="6"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		option = input ("")
+if (option=="7"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		option = input ("")
+if (option== "4"):
+	print ("MTN 60 Days Plan\n1.30GB =8000\n2.75GB=20000\n3.120GB=30000\n0.Back")
+	option = input ("")
+if (option=="1"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="2"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		ption = input ("")
+if (option=="3"):
+	print ("Enter your four digits pin")
+	option = input ("")
+if (option== "2244"):
+		print ("You've successfully purchased a Monthly Data plan. Thank you!")
+		
+
+	
